17476818f8ea3bfa69792aad7850620d8e74cfdf
tkgaspar <tkgaspar@yahoo.de>
2021-05-03 20:25:27 +0300

    Secuurity JWT Token implemented

--- starter_code/src/main/java/com/example/demo/SareetaApplication.java
+++ starter_code/src/main/java/com/example/demo/SareetaApplication.java
@@ -1,17 +1,35 @@
 package com.example.demo;
 
+
+
+import com.example.demo.security.UserDetailsServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableJpaRepositories("com.example.demo.model.persistence.repositories")
 @EntityScan("com.example.demo.model.persistence")
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableWebSecurity
+
 public class SareetaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SareetaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SareetaApplication.class, args);
+    }
 
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public UserDetailsServiceImpl userDetailsService(){
+        return new UserDetailsServiceImpl();
+    }
 }
